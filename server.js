3403b7401e76fac225d4f41a256f337d469592b7
RonsterJ <luciann.code@gmail.com>
2021-08-13 12:09:10 -0700

    db connect

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config('.env');
 const express = require('express');
 const app = express();
+const db = require('./models')
 
 
 app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}`))
