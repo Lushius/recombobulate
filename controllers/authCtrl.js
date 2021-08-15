8a87cfee7de94d4e69f28d316d3ac1006d388763
RonsterJ <luciann.code@gmail.com>
2021-08-15 14:05:15 -0700

    jwt and cookie

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -1,40 +1,52 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models')
+const db = require('../models');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 router.get('/login', (req, res) => {
-    res.render('auth/login');
+    res.render('auth/login', {accessToken: req.accessToken});
 })
 router.get('/register', (req, res) => {
-    res.render('auth/register')
+    res.render('auth/register', {accessToken: req.accessToken})
 })
 
 router.post('/login', (req, res) => {
-    db.user.findOne({uName: req.body.uName}, (err, user) => {
+    db.user.findOne({uName: req.body.uName}, (err, foundUser) => {
         if(err) return console.log(err);
-        if(!user) return res.send('<h1>Username could not be found</h1>');
-        res.redirect('/')
+        if(!foundUser) return res.send('<h1>Username could not be found</h1>');
+        bcrypt.compare(req.body.password, foundUser.password, (err, match) => {
+            if(err) return console.log(err);
+            if(!match) return res.send('Incorrect password');
+            const user = {
+                uName: foundUser.uName,
+            }
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: 20});
+            res.cookie('authorization', accessToken)
+            res.redirect('/');
+        })
     })
 })
 
 router.post('/register', (req, res) => {
-    // Check duplicates email and username
-    //Gen salt
-    //Hash pass
-    //create user with hashed
-    //write session
-    //redirect
-    const {uName, email} = req.body;
-    db.user.findOne({uName}, (err, found) => {
+    db.user.findOne({uName: req.body.uName}, (err, found) => {
         if(err) return console.log(err);
         if(found) return res.send('<h1>Username already in use</h1>');
-        db.user.findOne({email}, (err, found) => {
+        db.user.findOne({email: req.body.email}, (err, found) => {
             if(err) return console.log(err);
             if(found) return res.send('<h1>Email already in use</h1>');
-            db.user.create(req.body, (err, newUser) => {
+            bcrypt.genSalt(10, (err, salt) => {
                 if(err) return console.log(err);
-                console.log(newUser)
-                res.redirect('/auth/login')
+                bcrypt.hash(req.body.password, salt, (err, hash) => {
+                    if(err) return console.log(err);
+                    const {fName, lName, uName, email, password} = req.body;
+                    const newUser = {fName, lName, uName, email, password: hash}
+                    db.user.create(newUser, (err, created) => {
+                        if(err) return console.log(err);
+                        console.log(created)
+                        res.redirect('/auth/login')
+                    })
+                })
             })
         })
     })
--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 require('dotenv').config('.env');
 const express = require('express');
 const app = express();
-const db = require('./models')
+const jwt = require('jsonwebtoken');
 
-app.use((req, res, next) => {
-    if (req.url != '/favicon.ico' && req.url != '/styles/main.css') {
-        console.log(`${req.method} - ${req.url} @ ${new Date().toLocaleTimeString()}`);
-    }
-    next();
-});
 
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use((req, res, next) => {
+    if (req.url != '/favicon.ico' && req.url != '/styles/main.css') {
+        console.log(`${req.method} - ${req.url} @ ${new Date().toLocaleTimeString()}`);
+        console.log(req.headers.cookie)
+    }
+    const authHeader = req.headers.cookie;
+    const token = authHeader && authHeader.split('=')[1];
+    console.log(token)
+    next();
+});
 
 // Controllers
 app.use('/auth', require('./controllers/authCtrl'));
 
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {accessToken: req.accessToken})
 });
 app.get('*', (req, res) => {
     res.send('<h1>Error 404: Page not found</h1>');
